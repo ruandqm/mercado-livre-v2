9f0a36046992dbef3d9fedc7cfad3fd885e63d6c
ruandqm <ruandqm@gmail.com>
2023-01-26 23:04:24 -0300

    5.2 bonus

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import FilterContext from '../../pages/Home/contexts/FilterContext'
 import './index.scss'
 
 export const Navbar = () => {
+    const { setSearch } = useContext(FilterContext)
+
+    const getSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value)
+    }
     return (
         <header className='mainHeader'>
             <div className="searchBar">
-                <input type="text" />
+                <input type="text" onChange={getSearch} />
             </div>
             <nav className='d-flex justify-content-center'>
                 <ul>
